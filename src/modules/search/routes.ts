caa8268099299aee8629ee8ab12fa3eae0e176df
Yassine Jlassi <yassinej696@gmail.com>
2025-08-21 21:07:53 +0200

    fixing all the production problems
    	modified:   tsconfig.json

--- src/modules/search/routes.ts
+++ src/modules/search/routes.ts
@@ -1,7 +1,8 @@
 // src/modules/search/routes.ts
 import { Hono } from 'hono';
 import { createOverwatchClient, OverwatchClient } from '@/lib/overwatch/client.js';
-import { createSearchHandlers } from '@/modules/search/handlers.js';
+
+import { createSearchHandlers } from './handlers.js';
 import { config } from '@/lib/config/api.js';
 
 const createSearchRoutes = () => {
