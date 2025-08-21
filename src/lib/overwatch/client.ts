be7e1171b4c3c337435a42866c3caf8573d748e0
Yassine Jlassi <yassinej696@gmail.com>
2025-08-21 20:11:17 +0200

    adding the vercel config

--- src/lib/overwatch/client.ts
+++ src/lib/overwatch/client.ts
@@ -1,6 +1,6 @@
 // src/lib/overwatch/client.ts
-import got, { Got, HTTPError } from 'got';
 import { OverwatchApiError } from '@/lib/utils/errors.js';
+import got, { Got, HTTPError } from 'got';
 
 export interface OverwatchClientConfig {
   baseUrl: string;
